e996902478c749c1f7e71849f80ea59c14189f0e
Deepak Biswal <dbiswal@ipost.com>
2021-03-03 17:04:11 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,70 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const commandLineArgs = require('command-line-args');
+const errorHandler = require('errorhandler');
+require('dotenv').config();
+
+const routes = require('./routes');
+
+const PORT = process.env.PORT || 3000;
+const optionDefinitions = [
+  { name: 'verbose', alias: 'v', type: Boolean },
+  { name: 'src', type: String, multiple: true, defaultOption: true },
+  { name: 'timeout', alias: 't', type: Number },
+];
+const args = commandLineArgs(optionDefinitions);
+const APP_ENV = args.src[0]
+  ? args.src[0].toString().split('=')[1]
+  : 'development';
+const isProduction = APP_ENV === 'production';
+
+// Create express app
+const app = express();
+
+// Config defaults...
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+if (!isProduction) app.use(errorHandler());
+
+// Connect to DB
+const mongoCloudConnectionSetting = {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+};
+
+mongoose.connect(
+  isProduction
+    ? process.env.PROD_MONGO_CLUSTER
+    : process.env.LOCAL_MONGO_CLUSTER,
+  mongoCloudConnectionSetting
+);
+
+// Use routes
+app.use(routes);
+
+// Catch 404 and process to error handler
+app.use((req, res, next) => {
+  const error = new Error('Not Found');
+  error.status = 404;
+
+  next(error);
+});
+
+// Error Handler
+// eslint-disable-next-line no-unused-vars
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  res.json({
+    error: {
+      message: error.message,
+      error: isProduction ? {} : error,
+    },
+  });
+});
+
+// Access the server by listening to the defined port
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
+});
